0e895e2a60cdc7c9b023cbb5eb406769b200c813
Suvisha Palla <72501806+Suvishapalla@users.noreply.github.com>
2025-06-29 18:05:36 -0700

    Update launch_predictor.py

--- model/launch_predictor.py
+++ model/launch_predictor.py
@@ -1,1 +1,12 @@
+import joblib
+
+def load_model_and_vectorizer():
+    model = joblib.load("model/model.pkl")
+    vectorizer = joblib.load("model/vectorizer.pkl")
+    return model, vectorizer
+
+def predict(model, vectorizer, input_text):
+    X = vectorizer.transform([input_text])
+    pred = model.predict(X)
+    return "Likely to Succeed" if pred[0] == 1 else "Unlikely to Succeed"
 
